c1b4788648e496b53efdd42d6b97d994edabf4fb
Jack Zgombic <69125339+jzgom067@users.noreply.github.com>
2025-06-04 21:08:15 +0000

    Fix model issues
    There was a small issue with the migration being inconsistent with the model. In addition, I added max_length to the character fields since Copilot was begging me to do it, despite there being no issues before.

--- api/models.py
+++ api/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class UserAccount(models.Model):
     user_account_id = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True, null=True)
-    password_hash = models.CharField(null=True)
-    display_name = models.CharField(max_length=63, null=True)
+    password_hash = models.CharField(max_length=255, null=True)
+    display_name = models.CharField(max_length=50, null=True)
     is_internal = models.BooleanField(default=False)
     is_guest = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
