b826b74e0e75e212b21c417e7836332e3794a350
Jack Zgombic <69125339+jzgom067@users.noreply.github.com>
2025-06-04 00:43:37 +0000

    Trim settings.py further
    After some more poking around, I realized that we don't need most of Django's built-in authentication features, so we're just going to go with our own.

--- api/settings.py
+++ api/settings.py
@@ -20,19 +20,12 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "api",
-]
+INSTALLED_APPS = ["api"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
 ]
 
 ROOT_URLCONF = "api.urls"
