458d49b8655eee1818d471fbde7e66bfc272977b
Jack Zgombic <69125339+jzgom067@users.noreply.github.com>
2025-06-05 01:57:38 +0000

    Add model for user event
    A very basic start, it might include some other fields later depending on how development progresses.

--- api/models.py
+++ api/models.py
@@ -29,3 +29,26 @@ class UserLogin(models.Model):
         UserAccount, on_delete=models.CASCADE, related_name="logins"
     )
     login_time = models.DateTimeField(auto_now_add=True)
+
+
+class UserEvent(models.Model):
+    user_event_id = models.AutoField(primary_key=True)
+    user_account = models.ForeignKey(
+        UserAccount, on_delete=models.CASCADE, related_name="events"
+    )
+    title = models.CharField(max_length=50)
+
+    class EventType(models.TextChoices):
+        GENERIC = "GENERIC", "Generic"
+        SPECIFIC = "SPECIFIC", "Specific"
+
+    date_type = models.CharField(
+        max_length=20,
+        choices=EventType.choices,
+    )
+    duration = models.PositiveSmallIntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [models.Index(fields=["user_account"])]
