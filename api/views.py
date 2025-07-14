266ea78e4f07e3cfff020bfec35f2ee96d569fbf
Jack Zgombic <69125339+jzgom067@users.noreply.github.com>
2025-07-14 00:15:59 +0000

    Add session token creation to register endpoint
    Now the user is logged in as soon as they register.

--- api/views.py
+++ api/views.py
@@ -2,10 +2,13 @@ from rest_framework.decorators import api_view
 from rest_framework import serializers
 from rest_framework.response import Response
 
-from .models import UserAccount
+from django.db import transaction
+
+from .models import UserAccount, UserSession
 from .utils import validate_password
 
 import bcrypt
+import uuid
 
 
 # Create your views here.
@@ -30,6 +33,8 @@ def register(request):
 
     # Hash the password before saving
     pwd_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    # Create a session token for the new user
+    session_token = str(uuid.uuid4())
     try:
         # Check if the email already exists
         if UserAccount.objects.filter(email=email).exists():
@@ -37,10 +42,28 @@ def register(request):
                 {"error": {"email": ["An account with this email already exists"]}},
                 status=400,
             )
-        UserAccount.objects.create(email=email, password_hash=pwd_hash, is_guest=False)
+
+        with transaction.atomic():
+            new_user = UserAccount.objects.create(
+                email=email, password_hash=pwd_hash, is_guest=False
+            )
+            UserSession.objects.create(
+                session_token=session_token, user_account=new_user
+            )
+
     except Exception as e:
         print(e)
         return Response(
             {"error": {"general": ["An unknown error has occurred"]}}, status=500
         )
-    return Response({"message": ["Registration successful"]})
+
+    response = Response({"message": ["Registration successful"]})
+    response.set_cookie(
+        key="account_sess_token",
+        value=session_token,
+        httponly=True,
+        secure=True,
+        samesite="Lax",
+        max_age=2592000,  # 30 days
+    )
+    return response
