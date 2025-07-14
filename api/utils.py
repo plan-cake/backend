e52446cd0f027e3d26a2cfa1ee9b839296718b03
Jack Zgombic <69125339+jzgom067@users.noreply.github.com>
2025-07-14 01:14:46 +0000

    Create require_auth decorator
    Part of the "modular, scalable, easy to work with backend" plan. It checks the cookies to authenticate the user, and refreshes the session token cookie at the end.

--- api/utils.py
+++ api/utils.py
@@ -1,3 +1,10 @@
+from datetime import timedelta, datetime
+import functools
+from rest_framework.response import Response
+
+from api.models import UserSession
+
+
 def validate_password(password):
     MIN_LENGTH = 8
     SPECIAL_CHARACTERS = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
@@ -15,3 +22,49 @@ def validate_password(password):
         errors.append("Password must contain at least one special character.")
 
     return errors
+
+
+def require_auth(func):
+    """
+    A decorator to check if the user is authenticated based on their cookies.
+
+    The `user` object is made available in the `request` argument after authentication.
+
+    This refreshes the session token cookie with the response.
+    """
+
+    @functools.wraps(func)
+    def wrapper(request, *args, **kwargs):
+        token = request.COOKIES.get("account_sess_token")
+        if not token:
+            return Response(
+                {"error": {"general": ["Authentication required"]}}, status=401
+            )
+        try:
+            # Delete sessions where last_used is more than 30 days ago
+            UserSession.objects.filter(
+                last_used__lt=datetime.now() - timedelta(days=30)
+            ).delete()
+            session = UserSession.objects.get(session_token=token)
+            session.save()  # To update last_used to now
+            request.user = session.user_account
+        except UserSession.DoesNotExist:
+            response = Response(
+                {"error": {"general": ["Authentication required"]}}, status=401
+            )
+            response.delete_cookie("account_sess_token")
+            return response
+
+        response = func(request, *args, **kwargs)
+        # Intercept the response to refresh the session token cookie
+        response.set_cookie(
+            key="account_sess_token",
+            value=token,
+            httponly=True,
+            secure=True,
+            samesite="Lax",
+            max_age=2592000,  # 30 days
+        )
+        return response
+
+    return wrapper
