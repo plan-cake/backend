9df4df683523155ffdc66aa6ecaeca94f2ff0132
Jack Zgombic <69125339+jzgom067@users.noreply.github.com>
2025-07-20 19:13:41 +0000

    Reorganize project structure
    This organizes all the endpoints into groups, where related endpoints are grouped together both by a folder structure and by a URL prefix.

---
+++ api/auth/views.py
@@ -0,0 +1,117 @@
+from rest_framework.decorators import api_view
+from rest_framework import serializers
+from rest_framework.response import Response
+
+from django.db import transaction
+
+from api.settings import SESS_EXP_SECONDS
+from api.models import UserAccount, UserSession
+from api.utils import validate_input
+from api.auth.utils import validate_password
+
+import bcrypt
+import uuid
+
+
+class AccountInfoSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
+
+
+@api_view(["POST"])
+@validate_input(AccountInfoSerializer)
+def register(request):
+    email = request.validated_data.get("email")
+    password = request.validated_data.get("password")
+
+    try:
+        # Check if the email already exists
+        if UserAccount.objects.filter(email=email).exists():
+            return Response(
+                {"error": {"email": ["An account with this email already exists"]}},
+                status=400,
+            )
+
+        # Then validate the password after
+        if errors := validate_password(password):
+            return Response({"error": {"password": errors}}, status=400)
+        pwd_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+        # Now start a transaction to make sure both database calls are successful
+        with transaction.atomic():
+            new_user = UserAccount.objects.create(
+                email=email, password_hash=pwd_hash, is_guest=False
+            )
+
+            session_token = str(uuid.uuid4())
+            UserSession.objects.create(
+                session_token=session_token, user_account=new_user
+            )
+
+    except Exception as e:
+        print(e)
+        return Response(
+            {"error": {"general": ["An unknown error has occurred"]}}, status=500
+        )
+
+    response = Response({"message": ["Registration successful"]})
+    response.set_cookie(
+        key="account_sess_token",
+        value=session_token,
+        httponly=True,
+        secure=True,
+        samesite="Lax",
+        max_age=SESS_EXP_SECONDS,
+    )
+    return response
+
+
+@api_view(["POST"])
+@validate_input(AccountInfoSerializer)
+def login(request):
+    email = request.validated_data.get("email")
+    password = request.validated_data.get("password")
+
+    try:
+        user = UserAccount.objects.get(email=email)
+        if not bcrypt.checkpw(password.encode(), user.password_hash.encode()):
+            return Response({"error": {"password": ["Incorrect password"]}}, status=400)
+
+        session_token = str(uuid.uuid4())
+        UserSession.objects.create(session_token=session_token, user_account=user)
+
+    except UserAccount.DoesNotExist:
+        return Response(
+            {"error": {"email": ["No account found with this email"]}}, status=404
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            {"error": {"general": ["An unknown error has occurred"]}}, status=500
+        )
+
+    response = Response({"message": ["Login successful"]})
+    response.set_cookie(
+        key="account_sess_token",
+        value=session_token,
+        httponly=True,
+        secure=True,
+        samesite="Lax",
+        max_age=SESS_EXP_SECONDS,
+    )
+    return response
+
+
+class PasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(required=True)
+
+
+@api_view(["POST"])
+@validate_input(PasswordSerializer)
+def check_password(request):
+    password = request.validated_data.get("password")
+
+    if errors := validate_password(password):
+        return Response({"error": {"password": errors}}, status=400)
+
+    return Response({"message": ["Password is valid"]})
