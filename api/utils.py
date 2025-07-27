8e70dcbc3313c371d228e750827f57056928edb8
Jack Zgombic <69125339+jzgom067@users.noreply.github.com>
2025-07-27 22:57:48 +0000

    Add guest creation logic to require_auth
    This is a big commit, but the logic had to be overhauled to introduce the guest logic.
    
    First, it checks to authenticate the account. If that doesn't succeed, the guest fallback initiates:
    - If a guest session exists, it uses that as the current user.
    - If a guest session does not exist, it creates a new guest and uses that as the new user.
    
    Guests are created with an infinite-duration session.

--- api/utils.py
+++ api/utils.py
@@ -2,8 +2,12 @@ from datetime import timedelta, datetime
 import functools
 from rest_framework.response import Response
 
-from api.models import UserSession
+from django.db import transaction
+
+from api.models import UserAccount, UserSession
 from api.settings import SESS_EXP_SECONDS, GENERIC_ERR_RESPONSE
+
+import uuid
 
 
 def require_auth(func):
@@ -17,40 +21,104 @@ def require_auth(func):
 
     @functools.wraps(func)
     def wrapper(request, *args, **kwargs):
-        token = request.COOKIES.get("account_sess_token")
-        if not token:
-            return Response(
-                {"error": {"general": ["Authentication required."]}}, status=401
-            )
-        try:
-            # Delete non-infinite sessions where last_used is further than the expiration time
-            UserSession.objects.filter(
-                is_infinite=False,
-                last_used__lt=datetime.now() - timedelta(seconds=SESS_EXP_SECONDS),
-            ).delete()
-            session = UserSession.objects.get(session_token=token)
-            session.save()  # To update last_used to now
-            request.user = session.user_account
-        except UserSession.DoesNotExist:
-            response = Response(
-                {"error": {"general": ["Session expired."]}}, status=401
-            )
-            response.delete_cookie("account_sess_token")
-            return response
-        except Exception as e:
-            print(e)
-            return GENERIC_ERR_RESPONSE
+        acct_token = request.COOKIES.get("account_sess_token")
+        acct_sess_expired = False
+        if acct_token:
+            try:
+                # Delete non-infinite sessions where last_used is further than the expiration time
+                UserSession.objects.filter(
+                    is_infinite=False,
+                    last_used__lt=datetime.now() - timedelta(seconds=SESS_EXP_SECONDS),
+                ).delete()
+                with transaction.atomic():
+                    session = UserSession.objects.get(session_token=acct_token)
+                    session.save()  # To update last_used to now
 
-        response = func(request, *args, **kwargs)
-        # Intercept the response to refresh the session token cookie
-        response.set_cookie(
-            key="account_sess_token",
-            value=token,
-            httponly=True,
-            secure=True,
-            samesite="Lax",
-            max_age=SESS_EXP_SECONDS,
-        )
+                # At this point the account is authenticated
+                request.user = session.user_account
+
+                response = func(request, *args, **kwargs)
+                # Intercept the response to refresh the session token cookie
+                response.set_cookie(
+                    key="account_sess_token",
+                    value=acct_token,
+                    httponly=True,
+                    secure=True,
+                    samesite="Lax",
+                    max_age=SESS_EXP_SECONDS,
+                )
+                return response
+            except UserSession.DoesNotExist:
+                acct_sess_expired = True
+            except Exception as e:
+                print(e)
+                return GENERIC_ERR_RESPONSE
+
+        # At this point the account session either expired or did not exist
+        guest_token = request.COOKIES.get("guest_sess_token")
+
+        if guest_token:
+            # Make sure the guest session token exists (it should)
+            try:
+                with transaction.atomic():
+                    session = UserSession.objects.get(session_token=guest_token)
+                    session.save()  # Update last_used
+
+                request.user = session.user_account
+                # Run the function
+                response = func(request, *args, **kwargs)
+                response.set_cookie(
+                    key="guest_sess_token",
+                    value=guest_token,
+                    httponly=True,
+                    secure=True,
+                    samesite="Lax",
+                    max_age=SESS_EXP_SECONDS,
+                )
+            except UserSession.DoesNotExist:
+                print(
+                    f"Guest session {guest_token} does not exist. Either something went wrong, or someone's doing something weird with the API."
+                )
+                response = Response(
+                    {"error": {"general": ["Guest session expired."]}}, status=401
+                )
+            except Exception as e:
+                print(e)
+                return GENERIC_ERR_RESPONSE
+        else:
+            # Create a guest user with an infinite session
+            try:
+                with transaction.atomic():
+                    guest_account = UserAccount.objects.create(is_guest=True)
+                    new_session_token = str(uuid.uuid4())
+                    guest_session = UserSession.objects.create(
+                        session_token=new_session_token,
+                        user_account=guest_account,
+                        is_infinite=True,
+                    )
+
+                request.user = guest_account
+                # Run the function
+                response = func(request, *args, **kwargs)
+                response.set_cookie(
+                    key="guest_sess_token",
+                    value=guest_session.session_token,
+                    httponly=True,
+                    secure=True,
+                    samesite="Lax",
+                    max_age=SESS_EXP_SECONDS,
+                )
+            except Exception as e:
+                print(e)
+                return GENERIC_ERR_RESPONSE
+
+        # Make sure to return a message if the account session expired
+        if acct_sess_expired:
+            SESS_EXP_MSG = "Account session expired."
+            if "message" in response.data:
+                response.data["message"].append(SESS_EXP_MSG)
+            else:
+                response.data["message"] = [SESS_EXP_MSG]
         return response
 
     return wrapper
