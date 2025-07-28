9dad0d7fe125a29588d1be0d0a3b4585f3efe70c
Jack Zgombic <69125339+jzgom067@users.noreply.github.com>
2025-07-28 02:20:14 +0000

    Fix response message format
    This is a tough format to keep track of without something strict to check it.
    
    Maybe I'll look into an output serializer.

--- api/views.py
+++ api/views.py
@@ -7,4 +7,4 @@ from api.utils import require_auth
 @api_view(["GET"])
 @require_auth
 def index(request):
-    return Response({"message": f"Hello, {request.user.email}!"})
+    return Response({"message": [f"Hello, {request.user.email}!"]})
