c362497c95441f7b6032c00fac80f86703864c4e
Jack Zgombic <69125339+jzgom067@users.noreply.github.com>
2025-08-13 21:39:38 +0000

    Add input validation for date event creation
    I decided to separate date events and weekday events into separate endpoints, to avoid juggling two separate sets of logic in one giant endpoint.

--- api/event/views.py
+++ api/event/views.py
@@ -1,16 +1,36 @@
 import logging
 
+from rest_framework import serializers
 from rest_framework.response import Response
 
-from api.utils import api_endpoint, require_auth
+from api.utils import (
+    MessageOutputSerializer,
+    api_endpoint,
+    require_auth,
+    validate_json_input,
+    validate_output,
+)
 
 logger = logging.getLogger("api")
 
 
+class DateEventCreateSerializer(serializers.Serializer):
+    title = serializers.CharField(required=True)
+    duration = serializers.IntegerField(required=False)
+    start_date = serializers.DateField(required=True)
+    end_date = serializers.DateField(required=True)
+    start_time = serializers.TimeField(required=True)
+    end_time = serializers.TimeField(required=True)
+    time_zone = serializers.CharField(required=True)
+    custom_code = serializers.CharField(required=False)
+
+
 @api_endpoint("POST")
 @require_auth
-def create_event(request):
+@validate_json_input(DateEventCreateSerializer)
+@validate_output(MessageOutputSerializer)
+def create_date_event(request):
     """
-    Creates an event with the given details.
+    Creates a 'date' type event that spans specific dates.
     """
-    return Response({"message": "Event created successfully."})
+    return Response({"message": "Event created successfully."}, status=201)
