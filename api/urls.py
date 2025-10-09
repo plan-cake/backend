161d38a1d6cbab16716fd24be9f5bbcff5cd22ae
jzgom067 <jzgombic3@gmail.com>
2025-10-09 13:25:30 -0400

    Remove index endpoint
    This was meant to be removed a while ago. Finally, it's gone before we move to production.

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
     path("docs/", include("api.docs.urls")),
     path("auth/", include("api.auth.urls")),
     path("event/", include("api.event.urls")),
