874ff83359867d3d27683a47b3aaecb6f36bd489
jzgom067 <jzgombic3@gmail.com>
2025-12-28 15:19:07 -0500

    Update dashboard date/time range calculation
    Now it calculates the start/end date/time based on the original event's time zone. It makes more sense this way to represent the full range that the event creator intended.

--- api/dashboard/utils.py
+++ api/dashboard/utils.py
@@ -6,23 +6,26 @@ from api.models import UserEvent
 from api.utils import get_event_type
 
 
-def format_event_info(event: UserEvent, time_zone: ZoneInfo):
+def format_event_info(event: UserEvent, user_time_zone: ZoneInfo):
     """
     Formats event info into a dictionary for the dashboard get endpoint.
 
     For query efficiency, the event's timeslots should be prefetched.
     """
     all_timeslots: list[datetime] = []
+    event_time_zone = ZoneInfo(event.time_zone)
 
     event_type = get_event_type(event.date_type)
+    # Sort the timeslots by the EVENT'S time zone to get the min/max of the creator
     match event.date_type:
         case UserEvent.EventType.SPECIFIC:
             all_timeslots = [
-                ts.timeslot.astimezone(time_zone) for ts in event.date_timeslots.all()
+                ts.timeslot.astimezone(event_time_zone)
+                for ts in event.date_timeslots.all()
             ]
         case UserEvent.EventType.GENERIC:
             all_timeslots = [
-                get_weekday_date(ts.weekday, ts.timeslot).astimezone(time_zone)
+                get_weekday_date(ts.weekday, ts.timeslot).astimezone(event_time_zone)
                 for ts in event.weekday_timeslots.all()
             ]
 
@@ -34,13 +37,27 @@ def format_event_info(event: UserEvent, time_zone: ZoneInfo):
     # End time should be 15 minutes after the last timeslot
     end_time = (datetime.combine(datetime.min, end_time) + timedelta(minutes=15)).time()
 
+    # Then convert back to the user's local time zone for display
+    # datetime.combine has no time zone info, so we include the event's time zone to
+    # make sure it doesn't convert twice
+    start_datetime = (
+        datetime.combine(start_date, start_time)
+        .replace(tzinfo=event_time_zone)
+        .astimezone(user_time_zone)
+    )
+    end_datetime = (
+        datetime.combine(end_date, end_time)
+        .replace(tzinfo=event_time_zone)
+        .astimezone(user_time_zone)
+    )
+
     data = {
         "title": event.title,
         "event_type": event_type,
-        "start_date": start_date,
-        "end_date": end_date,
-        "start_time": start_time,
-        "end_time": end_time,
+        "start_date": start_datetime.date(),
+        "end_date": end_datetime.date(),
+        "start_time": start_datetime.time(),
+        "end_time": end_datetime.time(),
         "event_code": event.url_code.url_code,
         "time_zone": event.time_zone,
     }
